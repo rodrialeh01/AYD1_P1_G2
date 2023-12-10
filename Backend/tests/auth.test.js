2c688693a9b502d8139778c2f9bc3bdb935346c1
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-12-10 15:35:46 -0600

    Test crud usuarios y correcciones

--- Backend/tests/auth.test.js
+++ Backend/tests/auth.test.js
@@ -1,6 +1,8 @@
 import app from "../src/app.js";
 import request from "supertest";
 
+let idUser;
+
 describe("Test the singup path", () => {
     test("It should response with a 200 status code", async () => {
         const response = await request(app).post("/auth/sign/up").send(
@@ -59,7 +61,37 @@ describe("Test the singin path", () => {
                 "email": "test@gmail.com",
                 "password": "123456",
             }
-        ); 
+        );
+        idUser = response.body.data._id;
         expect(response.body.message).toBe("User logged in successfully");
     });
+});
+
+
+describe("Test the CRUD user", () => {
+    test("UPDATE It should response with a message User updated successfully", async () => {
+
+        const response = await request(app).patch(`/user/update/${idUser}`).send(
+            {
+                "name": "test1",
+                "lastName": "test1",
+                "phone": "123456789",
+                "email": "test1@gmail.com",
+                "birthDay": "2022-01-01",
+                "password": "1234567"
+            }
+        );
+        expect(response.body.message).toBe("User updated successfully");
+    });
+
+    test("GET it should response with a data of the user", async () => {
+        const response = await request(app).get(`/user/${idUser}`).send();
+        expect(response.body.data).toBeDefined();
+    });
+
+    test("DELETE it should response with a message User deleted successfully", async () => {
+        const response = await request(app).delete(`/user/delete/${idUser}`).send();
+        expect(response.body.message).toBe("User deleted successfully");
+    });
+
 });
