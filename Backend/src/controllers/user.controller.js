01f930cdf41a2bc8a4c544ed7323303053e6e50b
Daniel Dubón <3179425811504@ingenieria.usac.edu.gt>
2023-12-11 17:39:05 -0600

    Endpoit getUsers

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -64,15 +64,12 @@ export const deleteUser = async (req, res) => {
     }
 };
 
-// get the rented and purchased books of a user
 export const getBooks = async (req, res) => {
     try{
         const { id } = req.params;
 
         const booksUser = await User.findOne({ _id:id }, { rentedBooks: 1, purchasedBooks: 1 });
-        // get info of books rented
         const rentedBooks = await Book.find({ _id: { $in: booksUser.rentedBooks } }, { __v: 0 });
-        // get info of books purchased
         const purchasedBooks = await Book.find({ _id: { $in: booksUser.purchasedBooks } }, { __v: 0 });
 
         res.response({ rentedBooks, purchasedBooks }, 'Books of user', 200);
@@ -80,6 +77,18 @@ export const getBooks = async (req, res) => {
     }
     catch (error) {
         console.log(error);
+        res.response(null, error.message, 400);
+    }
+};
+
+// getAllUsers
+export const getAllUsers = async (req, res) => {
+    try{
+        const users = await User.find({}, { __v: 0, password: 0 });
+
+        res.response(users, 'Users', 200);
+
+    } catch (error) {
         res.response(null, error.message, 400);
     }
 };
